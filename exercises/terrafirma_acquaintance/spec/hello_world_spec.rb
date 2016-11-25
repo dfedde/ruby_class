5a2fa091f4f1d270a9629a0f8dcdf423c3634e5e
Duncan Fedde <duncan@fedde.us>
2016-11-25 11:07:33 -0700

    fix spec

--- exercises/terrafirma_acquaintance/spec/hello_world_spec.rb
+++ exercises/terrafirma_acquaintance/spec/hello_world_spec.rb
@@ -2,8 +2,8 @@ require 'rspec'
 
 describe 'bin/terrafirma_acquaintance' do
   it 'will print "Hello World!!"' do
-    output = `bin/terrafirma_acquaintance`.chomp
-    expect(output).to eq 'Hello World!!'
+    output = `bin/terrafirma_acquaintance`
+    expect(output.chomp).to eq 'Hello World!!'
   end
 end
 
