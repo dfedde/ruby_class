6c51e4f60e037ce7d04580fb218c2d8a58db3c7f
Duncan Fedde <duncan@fedde.us>
2017-01-07 08:29:33 -0700

    Add mileage-calculator

---
+++ exercises/mileage/lib/mileage-calculator.rb
@@ -0,0 +1,2 @@
+def calculate_mileage(gallons, miles)
+end
---
+++ exercises/mileage/spec/hello_world_spec.rb
@@ -0,0 +1,54 @@
+require 'rspec'
+require_relative '../lib/mileage-calculator'
+
+describe 'bin/mileage-calculator' do
+  let(:distance) {10}
+  let(:gallons)  {5}
+  let(:input) {"#{distance}\n#{gallons}\n"}
+  let(:path) {File.expand_path('../../bin/mileage-calculator', __FILE__) }
+  subject {`echo '#{input}' | #{path}`}
+
+
+  it 'will ask for how far you drove' do
+    is_expected.to include 'how far did you drive? '
+  end
+
+  it 'will ask how may gallons of gas you used' do
+    is_expected.to include 'how many gallons of gas did you use? '
+  end
+
+  it 'it will tell you your mileage' do
+    is_expected.to match(/your mileage was 2\.0MPG/)
+  end
+
+  describe 'with diffrent numbers' do
+    let(:distance) {12}
+    let(:gallons)  {3}
+    it 'it will tell you your mileage' do
+      is_expected.to match(/your mileage was 4\.0MPG/)
+    end
+  end
+end
+
+describe 'lib/mileage-calculator.rb' do
+  let(:mileage)  {distance/gallons.to_f}
+  subject {calculate_mileage(gallons, distance)}
+
+  describe 'when distance is larger then gallons used' do
+    let(:distance) {6}
+    let(:gallons)  {12}
+
+    it 'will calculate mileage' do
+      is_expected.to eq mileage
+    end
+  end
+
+  describe 'when distance is smaller then gallons used' do
+    let(:distance) {10}
+    let(:gallons)  {5}
+
+    it 'will calculate mileage' do
+      is_expected.to eq mileage
+    end
+  end
+end
